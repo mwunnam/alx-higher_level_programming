068d6f9e61cb2e7abcf7a5e5b857eb86b14ed193
mwunnam <mwunnam@gmail.com>
2023-11-29 00:44:03 +0000

    correction on task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 for x in range( 0, 100):
-    if x < 10:
-        print("{:02}, ".format(x), end="")
-    elif x == 99:
+    print("{:02}, ".format(x), end="")
+    if x == 99:
         print("{}".format(x))
-    else:
-        print("{}, ".format(x), end="")
