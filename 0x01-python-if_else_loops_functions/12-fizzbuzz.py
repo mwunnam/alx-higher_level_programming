3d934ea48caec5d18ca70f2adbbdc1cc7d93939e
mwunnam <mwunnam@gmail.com>
2023-11-29 14:04:06 +0000

    correction on task 12

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -2,11 +2,14 @@
 
 def fizzbuzz():
     for x in range(1, 101):
-        if x % 3 == 0:
+        if x % 5 == 0 and x % 3 == 0:
+            print("FizzBuzz", end=" ")
+
+        elif x % 3 == 0:
             print("Fizz", end=" ")
+
         elif x % 5 == 0:
             print("Buzz", end=" ")
-        elif x % 5 == 0 and x % 3 == 0:
-            print("FizzBuzz", end=" ")
+
         else:
             print("{}".format(x), end=" ")
