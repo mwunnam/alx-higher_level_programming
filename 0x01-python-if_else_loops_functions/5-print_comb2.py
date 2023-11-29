5acfafd4786f7c72a8def56f074659dcfbb33f7d
mwunnam <mwunnam@gmail.com>
2023-11-29 00:57:33 +0000

    correction on  task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-for x in range( 0, 100):
-    print("{:02}, ".format(x), end="")
-    if x == 99:
-        print("")
+for x in range(1, 100):
+    print("{:02}".format(x), end=", " if x < 99 else "\n")
