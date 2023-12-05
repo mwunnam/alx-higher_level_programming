145c7c8d50a0a5d106eb9b139e5753fa85770ed1
mwunnam <mwunnam@gmail.com>
2023-12-05 13:41:16 +0000

    task 2 correction

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,10 +5,10 @@ if __name__ == "__main__":
     args = sys.argv
     count = len(args)
 
-    if count > 1:
-        for x in range(1, count):
-            print("{}: {}".format(x, args[x]))
-    else:
+    if count == 1:
         print("0 arguments.")
 
-    
+    else:
+        print("{} arguments:".format(count - 1))
+        for x in range(1, count):
+            print("{}: {}".format(x, args[x]))
