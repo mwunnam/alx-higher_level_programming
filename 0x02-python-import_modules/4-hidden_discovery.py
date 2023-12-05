11b4e3933fb816edd1d3ca787ea1531c601257d6
mwunnam <mwunnam@gmail.com>
2023-12-05 21:04:53 +0000

    correction on task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,7 +2,7 @@
 
 import hidden_4
 if __name__ == "__main__":
-  names = dir(hidden_4)
-  for x in names:
-    if '__' not in x:
-        print(x)  
+    names = dir(hidden_4)
+    for x in names:
+        if '__' not in x:
+            print(x)
