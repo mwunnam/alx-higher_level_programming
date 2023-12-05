7e405f61a5eeafd1b0a241ad9dfb13ad7aa69fea
mwunnam <mwunnam@gmail.com>
2023-12-05 11:52:11 +0000

    done with task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+if __name__ == "__main__":
+    args = sys.argv
+    count = len(args)
+
+    if count > 1:
+        for x in range(1, count):
+            print("{}: {}".format(x, args[x]))
+
+    
