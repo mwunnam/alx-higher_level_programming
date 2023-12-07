3e50574669d22001992d8ffeabbb5f834515990a
mwunnam <mwunnam@gmail.com>
2023-12-07 15:53:57 +0000

    done with task 6

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    rows = len(matrix)
+    for x in range(rows):
+        columns = len(matrix[x])
+        for j in range(columns):
+            print("{:d}".format(matrix[x][j]), end=", ")
+        print()
