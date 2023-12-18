c1c7114c99127dc4142e3f6a794e0997f854ec39
mwunnam <mwunnam@gmail.com>
2023-12-18 15:35:32 +0000

    done with task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    element_printed = 0
+
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
+            element_printed += 1
+
+    except IndexError:
+        print("index dosen't exit")
+
+    print()
+    return (element_printed)
