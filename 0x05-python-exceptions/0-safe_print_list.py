1a2a9bc38982be86c3e9a4856717b9313e375953
mwunnam <mwunnam@gmail.com>
2023-12-18 23:32:40 +0000

    correction on task 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -7,7 +7,7 @@ def safe_print_list(my_list=[], x=0):
             print("{}".format(my_list[i]), end="")
             element_printed += 1
 
-    except:
+    except IndexError:
         pass
 
     print()
