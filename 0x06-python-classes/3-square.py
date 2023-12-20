2d8866e80c022f7f1d54b939b0ee733b051ffb8e
mwunnam <mwunnam@gmail.com>
2023-12-20 20:51:54 +0000

    done with task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""This module a module that define a class quare with attribute size."""
+
+
+class Square():
+    """
+    Initialize a sqare instance.
+
+    Args:
+        size (int): The size of the square.
+
+    Raises:
+        TypeError: if size is not an integer.
+        ValueError: if size is less than 0.
+    """
+
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+
+        elif size < 0:
+            raise ValueError("size must >= 0")
+
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
