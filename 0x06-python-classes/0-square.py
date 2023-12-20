5d150bbce3580ddab8bb73fd8e3eafe4e2dd0752
mwunnam <mwunnam@gmail.com>
2023-12-20 19:37:19 +0000

    done with task 1

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """ This is a module that defines a simple class."""
+
+
 class Square:
     """ This class represents a square currently has no attributes
         or methods."""
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module a module that define a class quare with attribute size"""
+
+
+class Square():
+    """This is a class square with a size attribute"""
+
+    def __init__(self, size=0):
+        self.__size = size
