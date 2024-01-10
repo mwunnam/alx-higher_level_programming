f3f32d1b6ced478daf404d4f740b0c563f02c09a
mwunnam <mwunnam@gmail.com>
2024-01-10 02:59:40 +0000

    done with task 6

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -11,5 +11,5 @@ def load_from_json_file(filename):
     """
     import json
 
-    with open(filename, mode="r", encoding="utf-8") as file:
-        return json.dump(file)
+    with open(filename, encoding="utf-8") as file:
+        return json.load(file)
