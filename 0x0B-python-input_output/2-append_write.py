79a60826ad8ee0bef7946521a2de90aa3418a452
mwunnam <mwunnam@gmail.com>
2024-01-10 01:51:27 +0000

    correction

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,15 +1,13 @@
 #!/usr/bin/python3
-""" Function that writes into a file """
+""" Function that appends into a file """
 
 
 def append_write(filename="", text=""):
-    """ Function writes as string into a file and returns the number
+    """ Function appens a string into a file and returns the number
         of characters written
     parameters:
         -filename (str): the name of the file to written in
-        -text (str): text to be written
+        -text (str): text to be appended
     """
     with open(filename, mode="a", encoding="utf-8") as file:
-        count = file.wirte(text)
-
-    return count
+        return file.wirte(text)
