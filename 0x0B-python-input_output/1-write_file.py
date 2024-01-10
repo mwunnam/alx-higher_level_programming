a5687c642959df17b021ea7ca89d9f3ec2f0b2fa
mwunnam <mwunnam@gmail.com>
2024-01-10 01:19:26 +0000

    starting task 0x0B ground work done task 0 also done

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def read_file(filename=""):
+    """ Read a text file (UTF9) and print its content of stdout
+
+    parameters:
+    filename (str): The name of the file to be read, Default is
+                    empty strings
+    Returns:
+            None
+    """
+
+    with open(filename, mode="r", encoding="utf-8) as file:
+        text = file.read()
+        print(content)
