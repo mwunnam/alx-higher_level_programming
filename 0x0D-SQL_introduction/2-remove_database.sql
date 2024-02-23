4e19c05c0d6f6483a2fd2cc0a52971518d1ba623
mwunnam <mwunnam@gmail.com>
2024-02-23 22:21:00 +0000

    correction on task 2

--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,3 +1,3 @@
 -- This deletes the database in my MySQL Server.
 -- If Database doesn't exit, the script should not fall.
-DROP DATABASE IF EXISTS `htbn_0c_0`;
+DROP DATABASE IF EXISTS `hbtn_0c_0`;
