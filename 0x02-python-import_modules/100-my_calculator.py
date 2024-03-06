a8f87033d48647eb13a9abff11d879de94d4829c
mwunnam <mwunnam@gmail.com>
2024-03-06 22:40:10 +0000

    done with task 6

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,29 @@
+#!usr/bin/env python3
+
+import sys
+from calculator_1 import add, sub, div, mul
+
+if len(sys.argv) != 4:
+    print("Usage: ./100-my_calculator.py <a> <operator> <b> ")
+    exit 1
+
+a = int(sys.argv[1])
+operator = sys.argv[2]
+b = int(sys.argv[3])
+
+if operator == '+':
+    result = add(a, b)
+
+elif operator == '-':
+    result = sub(a, b)
+
+elif operator == '*':
+    result = mul(a, b)
+
+elid operator == '/':
+    result = div(a, b)
+
+else 
+    print("Unknown operator. Available operators: +, -, * and /")
+
+print(f"{a} {operator} = {result}")
