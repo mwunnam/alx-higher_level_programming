8c6b1d9dbbae28dd1b625ce585159f26e3d3c04e
mwunnam <mwunnam@gmail.com>
2024-03-07 19:10:57 +0000

    correction on task 9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,5 +1,4 @@
 -- Creating a table  in the database
-
 CREATE TABLE IF NOT EXISTS `second_table`
 (id INT, `name` VARCHAR(256), `score` INT);
 INSERT INTO `second_table` (`id`, `name`, `score`) VALUES (1, "John", 10);
