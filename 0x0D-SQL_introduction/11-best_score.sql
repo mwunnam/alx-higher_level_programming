82644ba38495cbb9c9f5908100af780f94bb7bc3
mwunnam <mwunnam@gmail.com>
2024-03-07 20:12:26 +0000

    done with task 12

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,5 +1,5 @@
 -- Selecting score >= 10
 SELECT `score`, `name`
 FROM `second_table`
-WHERE `score` >= 10;
+WHERE `score` >= 10
 ORDER BY `score` DESC;
---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,4 @@
+-- Updating a score using name value
+UPDATE `second_table`
+SET `score` = 10
+WHERE `name` = "Bob";
