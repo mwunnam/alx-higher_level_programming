bafeaa1f2a616bbe635dbb00782e1b5cf9466a9a
mwunnam <mwunnam@gmail.com>
2024-03-11 16:09:31 +0000

    done with task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const sentence = ['c is fun', 'Python is cool', 'JavaScript is amzing'];
+
+let index = 0;
+
+while (index < sentence.length) {
+  console.log(sentence[index]);
+  index++;
+}
