f267d11b6cb6aedd237a0c0b2ba1fc3c607b7570
mwunnam <mwunnam@gmail.com>
2024-03-11 22:51:57 +0000

    correction on task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,9 +1,12 @@
 #!/usr/bin/node
+if (isNaN(Number(process.argv[2]))) {
+  console.log('Missing size');
+}
 const x = Number(process.argv[2]);
 for (let i = 0; i < x; i++) {
   let row = '';
   for (let j = 0; j < x; j++) {
-    row += 'x ';
+    row += 'x';
   }
   console.log(row);
 }
