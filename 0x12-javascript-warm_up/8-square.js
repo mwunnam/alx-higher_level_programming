5fcd2a265a0a6bfb544f9598251170bc9d17fa8e
mwunnam <mwunnam@gmail.com>
2024-03-14 04:05:50 +0000

    correction on tak 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
-if (isNaN(Number(process.argv[2])) || Number(process.argv[2] < 0)) {
+if (isNaN(Number(process.argv[2])) || process.argv[2] === undefined) {
   console.log('Missing size');
 }
 const x = Number(process.argv[2]);
-let i, j;
-for (i = 0; i < x; i++) {
+for (let i = 0; i < x; i++) {
   let row = '';
-  for (j = 0; j < x; j++) {
-    row += 'x';
+  for (let j = 0; j < x; j++) {
+    row += 'X';
   }
   console.log(row);
 }
