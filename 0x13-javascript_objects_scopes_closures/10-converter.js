e4e2cd72c46d878b1fa3ad7639094447a0320c68
mwunnam <mwunnam@gmail.com>
2024-03-14 01:53:13 +0000

    correction

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -2,7 +2,11 @@
 exports.converter = function (base) {
   return function convertToBase (num) {
     if (num < base) {
-      return '' + num;
+      if (base > 10 && num > 9) {
+        return String.fromCharCode(65 + num - 10);
+      } else {
+        return '' + num;
+      }
     } else {
       return convertToBase(Math.floor(num / base)) + (num % base);
     }
