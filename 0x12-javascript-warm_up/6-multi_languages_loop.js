90af83890014d7924f9dd8b5ff98be6c05848a93
mwunnam <mwunnam@gmail.com>
2024-03-14 03:59:26 +0000

    correction on task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,9 +1,15 @@
 #!/usr/bin/node
+/*
 const sentence = ['c is fun', 'Python is cool', 'JavaScript is amzing'];
 
 let index = 0;
-
+*/
+for (const sentence of ['C is fun', 'Python is cool', 'JavaScript is amzing']
+) {
+  console.log(sentence);
+}
+/*
 while (index < sentence.length) {
   console.log(sentence[index]);
   index++;
-}
+} */
