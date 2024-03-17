727e0b0a35be758c4e8dd527e40c7c0d0bbd25b5
mwunnam <mwunnam@gmail.com>
2024-03-17 19:51:25 +0000

    done with task 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import MySQLdb
+"""
+A script which list all states fomr the database
+"""
+
+
+def list_states(username, passwd, database):
+    """
+    Function that list all the sate in the database
+    usernama: The name of the user
+    passwd: The password
+    database: The name of the database
+    """
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                         passwd=passwd, db=database)
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDERED BY id ASC")
+    rows = cursor.fetchall()
+
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__"
