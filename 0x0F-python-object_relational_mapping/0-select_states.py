b83a16edc1503c78e6d864454ffbc6c793d617c5
mwunnam <mwunnam@gmail.com>
2024-03-17 21:12:15 +0000

    correction on task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,34 +1,27 @@
 #!/usr/bin/python3
 import MySQLdb
-
+import sys
 
 """
 A script which list all states fomr the database
 """
 
-
-def list_states(username, passwd, database):
-    """
-    Function that list all the sate in the database
-    usernama: The name of the user
-    passwd: The password
-    database: The name of the database
-    """
-    db = MySQLdb.connect(host="localhost", port=3306, user=username,
-                         passwd=passwd, db=database)
-    cursor = db.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-    rows = cursor.fetchall()
-
-    for row in rows:
-        print(row)
-
-    cursor.close()
-    db.close()
-
-
 if __name__ == "__main__":
-    username = "your_username"
-    passwd = "your_password"
-    database = "your_database"
-    list_states(username, passwd, database)
+    def list_states(username, passwd, database):
+        """
+        Function that list all the sate in the database
+        usernama: The name of the user
+        passwd: The password
+        database: The name of the database
+        """
+        db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                             passwd=sys.argv[2], db=sys.argv[3])
+        cursor = db.cursor()
+        cursor.execute("SELECT * FROM states ORDER BY id ASC")
+        rows = cursor.fetchall()
+
+        for row in rows:
+            print(row)
+
+        cursor.close()
+        db.close()
