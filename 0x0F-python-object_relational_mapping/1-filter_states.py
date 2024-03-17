2323e1f84064e7c6a4265cca08e8aef3c9bff80f
mwunnam <mwunnam@gmail.com>
2024-03-17 22:02:48 +0000

    correction

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -6,10 +6,10 @@ This script list only states starting with N
 """
 
 if __name__ == "__main__":
-    db = db.connect(host="localhost", port=3366, user=sys.argv[1],
+    db = db.connect(host="localhost", port=3306, user=sys.argv[1],
                     passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE state_name LIKE BINARY 'N%' ORDER BY
+    cursor.execute("SELECT * FROM states WHERE state_name LIKE 'N%' ORDER BY
                    state.id ASC")
     rows = cursor.fetchall()
     for row in rows:
