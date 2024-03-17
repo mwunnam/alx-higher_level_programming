3ccd219f139d364cfeb555d440b0864749af110c
mwunnam <mwunnam@gmail.com>
2024-03-17 22:28:13 +0000

    correctoin

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,7 +7,7 @@ import MySQLdb
 
 
 if __name__ == "__main__":
-    db = db.connect(host="localhost", user=sys.argv[1],
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
                     passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY
