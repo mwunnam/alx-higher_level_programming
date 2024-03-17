edc85a41acb8364d46e85f9e95f9dc75ec7ec596
mwunnam <mwunnam@gmail.com>
2024-03-17 22:12:32 +0000

    correctoin

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,10 +9,11 @@ if __name__ == "__main__":
     db = db.connect(host="localhost", port=3306, user=sys.argv[1],
                     passwd=sys.argv[2], db=sys.argv[3])
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE state_name LIKE 'N%' ORDER BY
-                   state.id ASC")
     rows = cursor.fetchall()
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY
+                   states.id ASC")
     for row in rows:
         print(row)
+
     cursor.close()
     db.close()
