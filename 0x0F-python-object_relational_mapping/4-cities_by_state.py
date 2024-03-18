703c5e6b7b9349e90c7ca39311edc50384f9f548
mwunnam <mwunnam@gmail.com>
2024-03-18 01:04:19 +0000

    still correction on task 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
                          passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cursor = db.cursor()
     cursor.execute("""SELECT cities.id, cities.name, states.name FROM cities
-            INNER JOINT state ON states.id=cities.state_id""")
+            INNER JOIN states ON states.id=cities.state_id""")
     rows = cursor.fetchall()
 
     for row in rows:
