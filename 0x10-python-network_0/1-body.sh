79afa97d49ac4fe89681cba8d93753a48f00ef08
mwunnam <mwunnam@gmail.com>
2024-04-05 12:33:43 +0100

    correction on task 1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This is to use GET request to URL and displays body of the response with http
-resp=$(curl -s -w "%{http_code}" "$1") && [ "$resp" == "200" ] && echo "$resp"
+curl -s -w "%{http_code}" -o /dev/null -I "$1"
