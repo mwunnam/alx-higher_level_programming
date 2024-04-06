97b0d71e98e733716be079dafcd1667842ef3a6b
mwunnam <mwunnam@gmail.com>
2024-04-06 23:22:27 +0100

    correction on task 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This is to send email with subject using POST
-curl -X POST -s -H "email: test@gmail.com" -H "subject: I will always be here for PLD" "$!"
+curl -s -X POST -d "email=test@gmail.com&subject=I will always be here for PLD" "$!"
