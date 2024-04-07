3a4a18b2c1223fa28ce3746234d4d651fddeae13
mwunnam <mwunnam@gmail.com>
2024-04-07 02:30:07 +0100

    correction

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This is to make a request to a server that cause a respose You got me!
-curl -sL 0.0.0.0:5000/catch_me -X PUT -H "Origin:HolbertonSchool"
+curl -sL 0.0.0.0:5000/catch_me_3 -X PUT -H "Origin:HolbertonSchool"
