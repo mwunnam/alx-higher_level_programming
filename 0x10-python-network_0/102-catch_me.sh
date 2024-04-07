4ab21419dbdbea440e86615e92c179286e6d4478
mwunnam <mwunnam@gmail.com>
2024-04-07 02:21:32 +0100

    correction

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This is to make a request to a server that cause a respose You got me!
-curl -sL -X POST 0.0.0.0:5000/catch_me
+curl -sL 0.0.0.0:5000/catch_me -H "Message:You got me!"
