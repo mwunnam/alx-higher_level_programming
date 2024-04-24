65d8adb7a4a1879792ebd1e15dfc2441d42ec21f
mwunnam <mwunnam@gmail.com>
2024-04-24 12:46:44 +0000

    done with task 4

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const apiUrl = process.argv[2];
+const characterId = '18';
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+  }
+  const films = JSON.parse(body).results;
+  const wedgeAntillesMovies = films.filter(film =>
+    film.characters.includes('https://swapi-api.alx-tools.com/api/people/' +
+    characterId + '/'));
+  console.log(wedgeAntillesMovies.length);
+});
