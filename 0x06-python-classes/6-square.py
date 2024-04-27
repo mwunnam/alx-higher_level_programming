369b9c2d3d70df23f37314eda9a693d6715fbb3f
mwunnam <mwunnam@gmail.com>
2024-04-27 16:03:20 +0000

    correction

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -59,7 +59,8 @@ class Square:
         if self.__size == 0:
             print()
             return
-        for _ in range(self.__position[1]):
-            print()
+        if self.__position[1] > 0:
+            print("\n" * self.__position[1], end="")
+
         for _ in range(self.__size):
             print(" " * self.__position[0] + "#" * self.__size)
